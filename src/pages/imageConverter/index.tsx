8eeeb141675070f9f09c7dc7c728eb4139177023
Bittu kumar <bittu@asiasoftwaresolutions.com>
2024-07-29 16:43:18 +0530

    Change text

--- src/pages/imageConverter/index.tsx
+++ src/pages/imageConverter/index.tsx
@@ -356,10 +356,8 @@ function ImageConverter(): JSX.Element {
         <div className="bg-gray-50 h-36 lg:h-full mx-5 rounded-lg"></div>
         <div className="lg:col-span-3 py-2 px-5">
           <div className="mb-12 text-center">
-            <h1 className="text-4xl font-bold">File Converter</h1>
-            <p className="text-sm pt-2">
-              Easily convert files from one format to another, online.
-            </p>
+            <h1 className="text-4xl font-bold">Image Converter</h1>
+            <p className="text-sm pt-2">Convert images online, for free.</p>
           </div>
           {!!uploadedFileList && !!uploadedFileList.length ? (
             <>
